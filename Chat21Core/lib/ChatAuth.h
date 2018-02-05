2320e6a8e02188cdb4ef2a6e4b251c9da4f59583
Andrea Sponziello <andrea.sponziello@gmail.com>
2018-02-05 16:59:55 +0100

    ChatAuth introduced

---
+++ Chat21Core/lib/ChatAuth.h
@@ -0,0 +1,17 @@
+//
+//  ChatAuth.h
+//  tilechat
+//
+//  Created by Andrea Sponziello on 05/02/2018.
+//  Copyright © 2018 Frontiere21. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+@import Firebase;
+@class ChatUser;
+
+@interface ChatAuth : NSObject
+
++(void)authWithEmail:(NSString *)email password:(NSString *)password completion:(void (^)(ChatUser *user, NSError *))callback;
+
+@end
---
+++ Chat21Core/lib/ChatAuth.m
@@ -0,0 +1,31 @@
+//
+//  ChatAuth.m
+//  tilechat
+//
+//  Created by Andrea Sponziello on 05/02/2018.
+//  Copyright © 2018 Frontiere21. All rights reserved.
+//
+
+#import "ChatAuth.h"
+#import "ChatUser.h"
+@import Firebase;
+
+@implementation ChatAuth
+
++(void)authWithEmail:(NSString *)email password:(NSString *)password completion:(void (^)(ChatUser *user, NSError *))callback {
+    [[FIRAuth auth] signInWithEmail:email password:password completion:^(FIRUser *user, NSError *error) {
+        if (error) {
+            NSLog(@"Firebase Auth error for email %@/%@: %@", email, password, error);
+            callback(nil, error);
+        }
+        else {
+            NSLog(@"Firebase Auth success. email: %@, emailverified: %d, userid: %@", user.email, user.emailVerified, user.uid);
+            ChatUser *chatuser = [[ChatUser alloc] init];
+            chatuser.userId = user.uid;
+            chatuser.email = user.email;
+            callback(chatuser, nil);
+        }
+    }];
+}
+
+@end
